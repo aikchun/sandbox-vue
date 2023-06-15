c25d7e724709b9cda7dc2568404222411f189859
aikchun <iam@simaikchun.com>
2023-06-15 15:14:20 +0800

    initial commit - learned emitting events

---
+++ src/components/BaseCounter.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 10
+    }
+  },
+  computed: {
+    title() {
+      return this.count > 10 ? 'big' : 'small'
+    }
+  },
+  methods: {
+    onIncrementClick() {
+      this.count += 1
+    }
+  }
+}
+</script>
+<template>
+  <h1>
+    {{ title }}
+  </h1>
+  <div>
+    {{ count }}
+    <div>
+      <button type="button" v-on:click="onIncrementClick">increment</button>
+    </div>
+  </div>
+</template>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['favorite-character']
+}
+</script>
+<template>
+  <div>
+    <div>{{ character }}</div>
+    <button type="button" @click="$emit('favorite-character', this.character)">&#11088;</button>
+  </div>
+</template>
