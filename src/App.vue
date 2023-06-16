2688e6d008f8363c793c84510443e66ce3a8cdb6
aikchun <iam@simaikchun.com>
2023-06-16 14:45:25 +0800

    Brand new app for async fetching.

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,5 @@
-<script>
-import BaseCounter from './components/BaseCounter.vue'
-import CharacterCard from './components/CharacterCard.vue'
-
-export default {
-  components: {
-    BaseCounter,
-    CharacterCard
-  },
-  data() {
-    return {
-      count: 10,
-      characters: [
-        {
-          name: 'Chandler Bing',
-          hobbies: ['Watch TV', 'Feed the ducks'],
-          isFavorite: false
-        },
-        {
-          name: 'Joey Tribbiani',
-          hobbies: ['Watch TV', 'Feed the ducks'],
-          isFavorite: false
-        }
-      ],
-      favoriteCharacters: []
-    }
-  },
-  methods: {
-    favoriteCharacter(payload) {
-      this.favoriteCharacters.push(payload)
-    }
-  }
-}
-</script>
-
+<script></script>
 <template>
-  <h1>Count: {{ count }}</h1>
-
-  <strong>Friends</strong>
-  <ul>
-    <li v-for="character in characters" :key="`${character.name}`">
-      <CharacterCard :character="character" @favorite-character="favoriteCharacter" />
-    </li>
-  </ul>
-  <h2>Favorites</h2>
-  <ul>
-    <li v-for="character in favoriteCharacters" :key="character.name">
-      {{ character.name }}
-    </li>
-  </ul>
-
-  <BaseCounter />
+  <h1>Brand new app</h1>
 </template>
+<style></style>
